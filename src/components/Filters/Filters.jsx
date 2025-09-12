10aa32f685fe7f0de1abc109eb0c5c5d4cf5df7d
BigBEE <165787884+IvanNesterenko2369@users.noreply.github.com>
2025-09-12 18:12:01 +0200

    Filters

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -3,81 +3,129 @@ import { useSelector, useDispatch } from 'react-redux';
 import { selectBrands } from '../../redux/brands/selectors';
 import { fetchCars } from '../../redux/cars/operations';
 import { resetCars, setFilters } from '../../redux/cars/slice';
+import Select from 'react-select';
+import { useState } from 'react';
+import { customSelectStylesBrand, customSelectStylesPrice } from './Styles';
 
 export default function Filters() {
-
     const dispatch = useDispatch();
-    
     const brands = useSelector(selectBrands);
+
+    const formatNumber = (value) => {
+         if (!value) return '';
+         const number = value.replace(/\D/g, '');
+         return Number(number).toLocaleString();
+    };
+
+    const parseNumber = (value) => value.replace(/,/g, '');
+
+    const [selectedBrand, setSelectedBrand] = useState(null);
+    const [selectedPrice, setSelectedPrice] = useState(null);
+    const [minMileage, setMinMileage] = useState('');
+    const [maxMileage, setMaxMileage] = useState('');
+
+    const brandOptions = brands.map(brand => ({
+        value: brand.toLowerCase(),
+        label: brand
+    }));
+
+    const priceOptions = [
+        { value: '30', label: '30' },
+        { value: '40', label: '40' },
+        { value: '50', label: '50' },
+        { value: '60', label: '60' },
+        { value: '70', label: '70' },
+        { value: '80', label: '80' },
+        { value: '90', label: '90' },
+        { value: '100', label: '100' }
+    ];
 
     const handleSearch = (e) => {
         e.preventDefault();
         
-        const formData = new FormData(e.target.form);
         const filters = {};
-
         filters.page = 1;
     
-        if (formData.get('brands')) filters.brand = formData.get('brands');
-        if (formData.get('rentalPrice')) filters.rentalPrice = formData.get('rentalPrice');
-        if (formData.get('minMileage')) filters.minMileage = formData.get('minMileage');
-        if (formData.get('maxMileage')) filters.maxMileage = formData.get('maxMileage');
+        if (selectedBrand) filters.brand = selectedBrand.value;
+        if (selectedPrice) filters.rentalPrice = selectedPrice.value;
+        if (minMileage) filters.minMileage = minMileage;
+        if (maxMileage) filters.maxMileage = maxMileage;
 
         dispatch(resetCars());
         dispatch(fetchCars(filters));
-        dispatch(setFilters(filters))
+        dispatch(setFilters(filters));
     };
 
     return (
         <form className={css.filters}>
+            <div className={css.brand}>
+                <label className={css.label} htmlFor="brand-select">Car brand</label>
+                <Select
+                    id="brand-select"
+                    value={selectedBrand}
+                    onChange={setSelectedBrand}
+                    options={brandOptions}
+                    placeholder="Choose a brand"
+                    styles={customSelectStylesBrand}
+                /></div>
+            <div className={css.price}>
+                <label className={css.label} htmlFor="rentalPrice-select">Price/ 1 hour</label>
+                <Select
+                    id="rentalPrice-select"
+                    value={selectedPrice}
+                    onChange={setSelectedPrice}
+                    options={priceOptions}
+                    placeholder="Choose a price"
+                    styles={customSelectStylesPrice}
+                    formatOptionLabel={(option, { context }) => {
+                        const style = {
+                            fontFamily: 'Manrope, sans-serif',
+                            fontWeight: 500,
+                            fontSize: '16px',
+                            lineHeight: '20px',
+                            color: 'var(--black)',
+                        };
 
-            <label for="brand-select">Car brand</label>
-            <select name="brands" id="brand-select">
-                <option value="" hidden>Choose a brand</option>
-                {brands.map(brand => (
-                    <option value={brand.toLowerCase()}>{brand}</option>
-                ))}
-            </select>
+                        if (context === 'value') {
+                            return <div style={style}>{`To $${option.value}`}</div>;
+                        }
 
-            <label for="rentalPrice-select">Price/ 1 hour</label>
-            <select name="rentalPrice" id="rentalPrice-select">
-                <option value="" hidden>Choose a price</option>
-                <option value="30">30</option>
-                <option value="40">40</option>
-                <option value="50">50</option>
-                <option value="60">60</option>
-                <option value="70">70</option>
-                <option value="80">80</option>
-                <option value="90">90</option>
-                <option value="100">100</option>
-            </select>
+                        return <div style={style}>{option.label || `To $${option.value}`}</div>;
+                    }}
 
-            <label for="mileage">Car mileage / km</label>
+                />
+            </div>
 
-            <input
-                placeholder="From"
-                type="text"
-                id="mileage"
-                name="minMileage"
-                required
-                minlength="3"
-                maxlength="7" />
-            <input
-                placeholder="To"
-                type="text"
-                id="mileage"
-                name="maxMileage"
-                required
-                minlength="3"
-                maxlength="7" />
-            
+            <div className={css.input}>
+                <label className={css.label} htmlFor="mileage">Car mileage / km</label>
+                
+                <div className={css.div}>
+
+                    <input
+                        className={css.from}
+                        placeholder="From"
+                        type="text"
+                        id="mileage"
+                        name="minMileage"
+                        value={minMileage ? formatNumber(minMileage) : ''}
+                        onChange={(e) => setMinMileage(parseNumber(e.target.value))}
+                        minLength="3"
+                        maxLength="7" />
+                    <input
+                        className={css.to}
+                        placeholder="To"
+                        type="text"
+                        id="mileage"
+                        name="maxMileage"
+                        value={maxMileage ? formatNumber(maxMileage) : ''}
+                        onChange={(e) => setMaxMileage(parseNumber(e.target.value))}
+                        minlength="3"
+                        maxLength="7" />
+                </div>
+            </div>
             <button onClick={handleSearch} className={css.btn}>
                 Search
             </button>
-
-
         </form>
-        
-
     );
 };
---
+++ src/components/Filters/Styles.jsx
@@ -0,0 +1,200 @@
+export const customSelectStylesBrand = {
+    control: (provided) => ({
+        ...provided,
+        width: '204px',
+        height: '44px',
+        border: 'none',
+        borderRadius: '12px',
+        backgroundColor: 'var(--dark-white)',
+        boxShadow: 'none',
+        paddingLeft: '16px',
+        fontFamily: 'Manrope, sans-serif',
+        fontWeight: '500',
+        fontSize: '16px',
+        lineHeight: '20px',
+        color: 'var(--black)',
+        caretColor: 'transparent',
+        '&:hover': {
+            border: 'none'
+        },
+        cursor: 'pointer',
+    }),
+    
+    valueContainer: (provided) => ({
+        ...provided,
+        padding: '0'
+    }),
+
+    placeholder: (provided) => ({
+        ...provided,
+        margin: '0',
+        fontFamily: 'Manrope, sans-serif',
+        fontWeight: '500',
+        fontSize: '16px',
+        lineHeight: '20px',
+        color: 'var(--black)'
+    }),
+
+    indicatorSeparator: () => ({
+        display: 'none'
+    }),
+
+    dropdownIndicator: (provided, state) => ({
+        ...provided,
+        padding: '8px',
+        '& svg': {
+            display: 'none' 
+        },
+        '&::after': {
+            content: '""',
+            width: '16px',
+            height: '16px',
+            backgroundImage: 'url("/sprite.svg#icon-arrow")', 
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundSize: 'contain',
+            display: 'block',
+            transform: state.menuIsOpen ? 'rotate(180deg)' : 'rotate(0deg)',
+            transition: 'transform 0.2s ease'
+        }
+    }),
+    menu: (provided) => ({
+        ...provided,
+        marginTop: '4px',
+        borderRadius: '12px',
+        border: '1px solid var(--dark-white)',
+        width: '204px',
+        backgroundColor: 'var(--white)',
+        overflow: 'hidden',
+        boxShadow: '0px 4px 36px 0px rgba(0, 0, 0, 0.02)'
+    }),
+    menuList: (provided) => ({
+        ...provided,
+        padding: '6px 0px 6px 18px',
+        maxHeight: '272px',
+        position: 'relative',
+        '::-webkit-scrollbar': {
+            width: '8px',
+            
+        },
+        '::-webkit-scrollbar-thumb': {
+            backgroundColor: 'var(--light-gray)',
+            borderRadius: '10px',
+        }
+    }),
+    option: (provided, state) => ({
+        ...provided,
+        fontFamily: 'Manrope, sans-serif',
+        fontWeight: '500',
+        fontSize: '16px',
+        lineHeight: '20px',
+        color: state.isFocused ? 'var(--black)' : 'var(--gray)',
+        backgroundColor: 'transparent',
+        borderRadius: '8px',
+        padding: '8px 0px',
+        cursor: 'pointer',
+        ':active': {
+        backgroundColor: 'transparent' 
+    },
+    })
+};
+
+export const customSelectStylesPrice = {
+        control: (provided) => ({
+        ...provided,
+        width: '196px',
+        height: '44px',
+        border: 'none',
+        borderRadius: '12px',
+        backgroundColor: 'var(--dark-white)',
+        boxShadow: 'none',
+        paddingLeft: '16px',
+        fontFamily: 'Manrope, sans-serif',
+        fontWeight: '500',
+        fontSize: '16px',
+        lineHeight: '20px',
+        color: 'var(--black)',
+        caretColor: 'transparent',
+        '&:hover': {
+            border: 'none'
+        },
+        cursor: 'pointer',
+    }),
+    valueContainer: (provided) => ({
+        ...provided,
+        padding: '0'
+    }),
+
+    placeholder: (provided) => ({
+        ...provided,
+        margin: '0',
+        fontFamily: 'Manrope, sans-serif',
+        fontWeight: '500',
+        fontSize: '16px',
+        lineHeight: '20px',
+        color: 'var(--black)'
+    }),
+
+    indicatorSeparator: () => ({
+        display: 'none'
+    }),
+
+    dropdownIndicator: (provided, state) => ({
+        ...provided,
+        padding: '8px',
+        '& svg': {
+            display: 'none' 
+        },
+        '&::after': {
+            content: '""',
+            width: '16px',
+            height: '16px',
+            backgroundImage: 'url("/sprite.svg#icon-arrow")', 
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundSize: 'contain',
+            display: 'block',
+            transform: state.menuIsOpen ? 'rotate(180deg)' : 'rotate(0deg)',
+            transition: 'transform 0.2s ease'
+        }
+    }),
+    menu: (provided) => ({
+        ...provided,
+        marginTop: '4px',
+        borderRadius: '12px',
+        border: '1px solid var(--dark-white)',
+        width: '196px',
+        backgroundColor: 'var(--white)',
+        overflow: 'hidden',
+        boxShadow: '0px 4px 36px 0px rgba(0, 0, 0, 0.02)'
+    }),
+    menuList: (provided) => ({
+        ...provided,
+        padding: '6px 0px 6px 18px',
+        maxHeight: '188px',
+        position: 'relative',
+        '::-webkit-scrollbar': {
+            width: '8px',
+            
+        },
+        '::-webkit-scrollbar-thumb': {
+            backgroundColor: 'var(--light-gray)',
+            borderRadius: '10px',
+        }
+    }),
+    option: (provided, state) => ({
+        ...provided,
+        fontFamily: 'Manrope, sans-serif',
+        fontWeight: '500',
+        fontSize: '16px',
+        lineHeight: '20px',
+        color: state.isFocused ? 'var(--black)' : 'var(--gray)',
+        backgroundColor: 'transparent',
+        borderRadius: '8px',
+        padding: '8px 0px',
+        cursor: 'pointer',
+        ':active': {
+        backgroundColor: 'transparent' 
+    },
+    })
+};
